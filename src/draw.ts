e0eb6568cd4d0f18d7ad77a0efb7ff3e3ae25cd8
Natalie Wagner <nate.o.wagner@gmail.com>
2026-01-13 18:24:03 -0500

    feat: add dark mode support

--- src/draw.ts
+++ src/draw.ts
@@ -12,7 +12,30 @@ const pieceColors: { [k: string]: string } = {
   X: "#8f52ae",
   N: "#ae44a1",
 };
-const backgroundColor = "#242424";
+
+const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+let backgroundColor = darkModeQuery.matches ? "#242424" : "#fff";
+
+darkModeQuery.addEventListener("change", (e) => {
+  backgroundColor = e.matches ? "#242424" : "#fff";
+});
+
+export function drawBoard(ctx: CanvasRenderingContext2D, board: Board) {
+  const longerSide = Math.max(ctx.canvas.height, ctx.canvas.width);
+  const squareCellHeight = longerSide / board.grid.length;
+  const squareCellWidth = longerSide / board.grid[0].length;
+  ctx.fillStyle = backgroundColor;
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  for (let row = 0; row < board.grid.length; row++) {
+    for (let col = 0; col < board.grid[row].length; col++) {
+      const cell = board.grid[row][col];
+
+      drawCell(ctx, col, row, cell, squareCellWidth, squareCellHeight);
+    }
+  }
+}
 
 function drawCell(
   ctx: CanvasRenderingContext2D,
@@ -38,10 +61,10 @@ function drawCell(
         }
       }
     } else {
-      ctx.fillStyle = "#fff";
+      ctx.fillStyle = "#eee";
     }
   } else {
-    ctx.fillStyle = "#fff";
+    ctx.fillStyle = "#eee";
   }
   ctx.beginPath();
   const pos1 = rotatePos(
@@ -98,6 +121,7 @@ function rotatePos(
 ) {
   let offsetX = x - halfWidth;
   let offsetY = y - halfHeight;
+  // Handle non square canvas sizes
   if (halfWidth > halfHeight) {
     offsetY -= halfWidth - halfHeight;
   } else {
@@ -109,101 +133,4 @@ function rotatePos(
     length * Math.cos(angle) + halfWidth,
     length * Math.sin(angle) + halfHeight,
   ];
-}
-
-export function drawBoard(ctx: CanvasRenderingContext2D, board: Board) {
-  const longerSide = Math.max(ctx.canvas.height, ctx.canvas.width);
-  const squareCellHeight = longerSide / board.grid.length;
-  const squareCellWidth = longerSide / board.grid[0].length;
-  ctx.fillStyle = backgroundColor;
-  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-  for (let row = 0; row < board.grid.length; row++) {
-    for (let col = 0; col < board.grid[row].length; col++) {
-      const cell = board.grid[row][col];
-
-      drawCell(ctx, col, row, cell, squareCellWidth, squareCellHeight);
-      // const directions = [
-      //   [0, 1],
-      //   [0, -1],
-      //   [1, 0],
-      //   [-1, 0],
-      // ];
-      // const walls = [];
-      // for (let i = 0; i < 4; i++) {
-      //   const direction = directions[i];
-      //   const newRow = row + direction[0],
-      //     newCol = col + direction[1];
-
-      //   if (
-      //     newRow < 0 ||
-      //     newRow >= board.grid.length ||
-      //     newCol < 0 ||
-      //     newCol >= board.grid[col].length
-      //   ) {
-      //     if (blocked == null) walls.push(i);
-      //     continue;
-      //   }
-      //   if (blocked !== board.grid[newRow][newCol]) {
-      //     walls.push(i);
-      //   }
-      // }
-      // ctx.strokeStyle = backgroundColor;
-      // if (blocked !== null) {
-      //   if (blocked.startsWith("Blocker")) {
-      //     const text = blocked.split(" ")[1];
-      //     ctx.font = "25px sans-serif";
-      //     ctx.textBaseline = "middle";
-      //     const textSize = ctx.measureText(text);
-      //     const xPos = col * width + (width - textSize.width) / 2;
-      //     const yPos = row * height + height / 2;
-      //     ctx.fillStyle = "#000";
-      //     ctx.fillText(text, xPos, yPos);
-      //   } else {
-      //     if (blocked === "Wall") {
-      //       ctx.fillStyle = backgroundColor;
-      //     } else {
-      //       if (pieceColors[blocked]) {
-      //         ctx.fillStyle = pieceColors[blocked];
-      //       } else {
-      //         ctx.fillStyle = pieceColors[blocked] =
-      //           "#" +
-      //           ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
-      //       }
-      //     }
-      //     ctx.beginPath();
-      //     ctx.rect(col * width, row * height, width + 1, height + 1);
-      //     ctx.fill();
-      //   }
-      // }
-
-      // for (let i = 0; i < walls.length; i++) {
-      //   const wall = walls[i];
-      //   ctx.beginPath();
-      //   switch (wall) {
-      //     case 0: {
-      //       ctx.moveTo((col + 1) * width, row * height);
-      //       ctx.lineTo((col + 1) * width, (row + 1) * height);
-      //       break;
-      //     }
-      //     case 1: {
-      //       ctx.moveTo(col * width, row * height);
-      //       ctx.lineTo(col * width, (row + 1) * height);
-      //       break;
-      //     }
-      //     case 2: {
-      //       ctx.moveTo(col * width, (row + 1) * height);
-      //       ctx.lineTo((col + 1) * width, (row + 1) * height);
-      //       break;
-      //     }
-      //     case 3: {
-      //       ctx.moveTo(col * width, row * height);
-      //       ctx.lineTo((col + 1) * width, row * height);
-      //       break;
-      //     }
-      //   }
-      //   ctx.stroke();
-      // }
-    }
-  }
 }
